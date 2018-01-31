c2f26444fc09ebee58fe4a77a6de71e0a6aa95e4
Mackenzie Thomas <mackenzie.thomas54@gmail.com>
2018-01-31 11:46:09 -0600

    Added Current Work

---
+++ Simple RPG/Assets/Scripts/ActionItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionItem :Interactable {
+
+	public override void Interact(){
+		Debug.Log ("Interacing with base item");
+	}
+}
